bc6056cd73448c0327e1ecfa671f9f1e9564bd1f
Akshay_M <harshaakshay14@gmail.com>
2023-11-22 00:12:05 +0530

    files added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import route from './Routes/routes.js';
 import cors from 'cors';
 import defalutdata from './default.js';
 import razorpay from "razorpay";
+import path from 'path';
+
+const __dirname=path.resolve();
 
 dotenv.config();
 const app=express();
@@ -16,6 +19,14 @@ app.use(json());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use("/",route)
+
+app.use(express.static(path.join(__dirname,"./client/build")));
+
+app.get('*',function(_, res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"),function(err){
+        res.status(500).send(err)
+    })
+})
 
 
 
