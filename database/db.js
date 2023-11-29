be74e43246e511ede1463cf69bb0f4482fd5d8df
Akshay_M <105048001+akshaym1412@users.noreply.github.com>
2023-11-29 11:38:27 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connection=(async(User,Password)=>{
-    const URL=`mongodb+srv://${User}:${Password}@cluster0.5tcyjvv.mongodb.net/?retryWrites=true&w=majority`
+    const URL=`mongodb+srv://${User}:${Password}@cluster0.5tcyjvv.mongodb.net/`
     try{
         await mongoose.connect(URL)
         console.log("Database Connected");
